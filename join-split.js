52a48c81397ab5a7b939975c5ebc2334a3a9505e
Masi <mas.ahmadii@yahoo.com>
2025-05-26 12:13:46 +0330

    feat: added join-split.js

---
+++ join-split.js
@@ -0,0 +1,7 @@
+const strings = ['a', 'b', 'c', 'd'];
+const sumOfString = strings.join('');
+console.log(sumOfString)
+
+const ListString = 'Ali, Masi, Javid';
+const newListArrayType = ListString.split();
+console.log(newListArrayType)
